4110b61959688e5cb90c38b3942ac8d6b67e57e1
RicardoAncona2 <rancona190@gmail.com>
2025-06-07 17:25:49 -0600

    fix error

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,25 +5,27 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TasksModule } from './tasks/tasks.module';
 import { ApolloDriver, } from '@nestjs/apollo';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [
-    GraphQLModule.forRoot({
-      driver: ApolloDriver,
-      autoSchemaFile: true,
-      playground: true,
-      introspection: true,
-      csrfPrevention: false, // disable CSRF protection
+  imports: [ConfigModule.forRoot({
+    isGlobal: true, // makes env available app-wide
+  }),
+  GraphQLModule.forRoot({
+    driver: ApolloDriver,
+    autoSchemaFile: true,
+    playground: true,
+    introspection: true,
+    csrfPrevention: false, // disable CSRF protection
 
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'postgres',
-      port: 5432,
-      url:process.env.DATABASE_URL,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true, // false in production
-    }),
+  }),
+  TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'postgres',
+    port: 5432,
+    url: process.env.DATABASE_URL,
+    entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    synchronize: true, // false in production
+  }),
     AuthModule,
     UsersModule,
     TasksModule,
