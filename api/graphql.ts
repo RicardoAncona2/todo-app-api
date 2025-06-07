b0785ecf0063ac0a5245a81cd4ea29e2a848b30e
RicardoAncona2 <rancona190@gmail.com>
2025-06-07 14:40:04 -0600

    vercel fix

---
+++ api/graphql.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from '../src/app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import serverless from 'serverless-http';
+import express from 'express';
+
+const expressApp = express();
+
+let cachedServer: any;
+
+async function bootstrap() {
+  if (!cachedServer) {
+    const app = await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+    app.enableCors();
+    await app.init();
+    cachedServer = serverless(expressApp);
+  }
+  return cachedServer;
+}
+
+export const handler = async (req: any, res: any) => {
+  const server = await bootstrap();
+  return server(req, res);
+};
