c385b39637d2bed9667c695491116a1cc5b4c277
RicardoAncona2 <rancona190@gmail.com>
2025-06-07 16:43:47 -0600

    fix module not found error in vercel2

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'tsconfig-paths/register';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -5,9 +6,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // <-- change to your frontend port if different
+    origin: '*',
     credentials: true,
   });
+
   const config = new DocumentBuilder()
     .setTitle('Todo App')
     .setDescription('API docs for your NestJS ToDo app')
